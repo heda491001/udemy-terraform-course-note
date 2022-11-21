4250b6710d7324d73ee3e2377ec0cc7ce318ebbb
heda491001 <heda491001@gmail.com>
2022-11-21 16:22:49 +0900

    update

---
+++ part2/2-9-helloworld-code/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  access_key = "AKIASJ422UWHONZ3E2LT"
+  secret_key = "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"
+  region     = "eu-central-1"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = "true"
+  tags = {
+    Name = "my-vpc-1"
+  }
+}
